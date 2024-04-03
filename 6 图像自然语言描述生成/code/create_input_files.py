4ce0d03861b512f2924af42599450d324419ab5f
zyy <zhaoyiyang_jaeger@163.com>
2024-04-03 16:00:57 +0800

    DeepLearning

--- 6 图像自然语言描述生成/code/create_input_files.py
+++ 6 图像自然语言描述生成/code/create_input_files.py
@@ -3,9 +3,9 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='/data3/zhangweiyi/coco2014/dataset_coco.json',
-                       image_folder='/data3/zhangweiyi/coco2014/',
+                       karpathy_json_path='/home/mw/project/coco2014/dataset_coco.json',   # downloaded from http://cs.stanford.edu/people/karpathy/deepimagesent/caption_datasets.zip
+                       image_folder='/home/mw/project/coco2014/',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/data3/zhangweiyi/coco2014/',
+                       output_folder='/home/mw/work/work_dir/coco/',
                        max_len=50)
